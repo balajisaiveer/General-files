eb09ce06434cf4685a0151ed7ae89d07032f6eaf
balajisaiveer <puppalabalaji@gmail.com>
2018-07-25 19:44:31 +0530

    every day update

--- automatingGit.sh
+++ automatingGit.sh
@@ -1,29 +1,30 @@
 #!/usr/bin/env bash
-
-#touch ~/Documents/Scripting/temp.txt
-#echo $PWD
-git status > ~/Documents/Scripting/temp.txt
-#cat ~/Documents/Scripting/a.txt
-#cat ~/Documents/Scripting/genralstatus.txt
+#-----------------------------------------------------------------------
+#Automation of Git for dily purposes
+git status > temp.txt
+#---------------------------------------------------------------------------
+#creating file equalling the gitstatus when no changes exsisting
 echo "On branch master
 Your branch is up to date with 'origin/master'.
 
 nothing to commit, working tree clean" > genralstatus.txt
-#git status > ~/Documents/Scripting/temp.txt
-#cat ~/Documents/Scripting/temp.txt
-#diff -q ~/Documents/Scripting/a.txt ~/Documents/Scripting/temp.txt
+#-------------------------------------------------------------------------------
 status=$(diff -q ~/Documents/Scripting/genralstatus.txt ~/Documents/Scripting/temp.txt)
-#status = status+"this"
-#echo $status
+
 if [ "$status" == "" ]
 then
- 	#git add .
- 	#git 
-	echo "balaji"
+	echo "-----------------------------------------"
+ 	echo "Nothing changed today"
+ 	echo "-----------------------------------------"
 else
 	git add .
 	git commit -m  "every day update"
 	git push origin master
-	echo "update"
+
+	echo "-----------------------------------------"
+ 	echo "Today changes in this repo are pushed"
+ 	echo "-----------------------------------------"
 fi
+rm temp.txt
 rm genralstatus.txt
+
