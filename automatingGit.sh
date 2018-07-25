22c1d94cb495afdc236720bdbb0ec0feb49c9db8
balajisaiveer <puppalabalaji@gmail.com>
2018-07-25 18:43:58 +0530

    every day update

--- automatingGit.sh
+++ automatingGit.sh
@@ -3,19 +3,18 @@
 #touch ~/Documents/Scripting/temp.txt
 #echo $PWD
 git status > ~/Documents/Scripting/temp.txt
-
 #cat ~/Documents/Scripting/a.txt
+#cat ~/Documents/Scripting/genralstatus.txt
+echo "On branch master
+Your branch is up to date with 'origin/master'.
 
-cat ~/Documents/Scripting/genralstatus.txt
+nothing to commit, working tree clean" > genralstatus.txt
 #git status > ~/Documents/Scripting/temp.txt
 #cat ~/Documents/Scripting/temp.txt
-
 #diff -q ~/Documents/Scripting/a.txt ~/Documents/Scripting/temp.txt
-
 status=$(diff -q ~/Documents/Scripting/genralstatus.txt ~/Documents/Scripting/temp.txt)
 #status = status+"this"
-echo $status
-
+#echo $status
 if [ "$status" == "" ]
 then
  	#git add .
